756453ce9b7ffb37a4eaa396c87b3edd4527b495
Wizard Nate <nathanielorozco42@gmail.com>
2025-03-11 13:59:28 -0500

    Everything is Broken yay

--- src/Boss.hpp
+++ src/Boss.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "Stats.hpp"
 #include "Entity.hpp"
 
 class Boss : public Entity
@@ -7,5 +7,19 @@ class Boss : public Entity
 public:
     void Start();
     void Update();
+    void Fight();
+
+    /// Monster Stats ///
+    CharStats bossStats;
+    
+    //bossStats.strength = 0; //random int + character level
+    //bossStats.dexterity = 0; 
+    //bossStats.wit = 0;
+    //bosstats.wisdom = 0;
+    //bossStats.health = 0; // + random int + character level
+    
+    //bossStats.exp = 0; //player recieves this upon death
+    //bossStats.gold = 0;
+
 private:
 };
