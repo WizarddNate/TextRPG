ae281480f95f7cf9afa6012aece2d5420514a0b4
Wizard Nate <nathanielorozco42@gmail.com>
2025-03-17 14:44:41 -0500

    MONSTER COMBAT COMPLETE !!!

--- src/Monster.cpp
+++ src/Monster.cpp
@@ -43,8 +43,8 @@ void Monster::GenerateStats()
         monsterStats.wisdom = 1 + (playerStats.level * 2) + rollD6(); 
         monsterStats.health = 2 + (playerStats.level * 2) + rollD6(); // + random int 
     
-        monsterStats.exp = 2 + (playerStats.level * 3) + rollD6(); //player recieves this upon death
-        monsterStats.gold = 1 + (playerStats.level * 3) + rollD6();
+        monsterStats.exp = 2 + (playerStats.level * 2) + rollD6(); //player recieves this upon death
+        monsterStats.gold = 1 + (playerStats.level * 2) + rollD6();
 }
 
 void Monster::Fight()
@@ -56,17 +56,22 @@ void Monster::Fight()
     //printf(monsterStats.health);
 
     //player chooses between regular and special attack
+    if (monsterStats.health <= 0)
+    {
+        printf("The monster has been beaten. The monster union demands that you let him go on a break now. \n");
+        return;
+    }
     char AttackInput;
     AttackInput = request_char("A great foe stands before you. Take your next Action Carefully. \n A for attack, S for speical attack");
 
     switch(AttackInput)
     {
         case 'A':
-            monsterStats.health = monsterStats.health - ((monsterStats.dexterity + rollD6()) - (playerStats.strength + rollD6()));
+            monsterStats.health = monsterStats.health - abs((monsterStats.dexterity + rollD6()) - (playerStats.strength + rollD6()));
             printf("You do %i points in strength damage, the monster has %i hp remaining \n", playerStats.strength, monsterStats.health);
             break;
         case 'S':
-            //monsterStats.health = monsterStats.health - ((monsterStats.wisdom + rollD6()) - (playerStats.wit + rollD6()));
+            monsterStats.health = monsterStats.health - abs((monsterStats.wisdom + rollD6()) - (playerStats.wit + rollD6()));
             printf("You do %i points in magic damage with your wit, the monster has %i hp remaining \n", playerStats.wit, monsterStats.health);
             break;
         default:
@@ -77,11 +82,15 @@ void Monster::Fight()
     //check if monster died from that attack
     if (monsterStats.health <= 0)
     {
+        playerStats.exp = playerStats.exp + monsterStats.exp;
+        playerStats.gold = playerStats.gold + monsterStats.gold;
+        playerStats.health = playerStats.health + ( 2 + rollD6());
+
         room->monsterCount--;
         //room->ClearLocation(tryPos);
         // m_monsters.erace(m_monsters.begin() + i) <-
         //remove monster from array: Monster.remove/erase(monster.beginning + index)
-        printf("the monster has been vanqiushed!\n");
+        printf("the monster has been vanqiushed!\n you got %i gold pieces, %i exp, and your health has been restored to %i points.\n", monsterStats.gold, monsterStats.exp, playerStats.health);
     }
     else //monsters turn to attack
     {
@@ -89,13 +98,13 @@ void Monster::Fight()
         if (flipCoin = 1)
         {
             //monster makes a normal attack
-            playerStats.health = playerStats.health - ((playerStats.dexterity + rollD6()) - (monsterStats.strength + rollD6()));
+            playerStats.health = playerStats.health - abs((playerStats.dexterity + rollD6()) - (monsterStats.strength + rollD6()));
             printf("The monster hits you for %i points in strength damage, you have %i hp remaining \n", monsterStats.strength, playerStats.health);
         }
         else
         {
             //monster makes a special attack
-            playerStats.health = playerStats.health - ((playerStats.wisdom + rollD6()) - (monsterStats.wit + rollD6()));
+            playerStats.health = playerStats.health - abs((playerStats.wisdom + rollD6()) - (monsterStats.wit + rollD6()));
             printf("The monster hits you for  %i points in magic damage with its wit, you have %i hp remaining \n", monsterStats.wit, playerStats.health);
         }
     }
@@ -104,7 +113,7 @@ void Monster::Fight()
         if (playerStats.health <= 0)
     {
         //handle death
-        printf("You have died.\n Final Level: %i\n Gold Amount: %i\n", playerStats.level, playerStats.exp);
+        printf("You have died.\n Final Level: %i\n Gold Amount: %i\n", playerStats.level, playerStats.gold);
         exit(0);
     }
 }
